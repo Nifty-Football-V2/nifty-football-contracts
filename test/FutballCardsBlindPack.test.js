0432d01e8e2e19c891ef3ca5f454d66545a4e564
Andy Gray <andy@whatsthescore.co.uk>
2019-02-09 18:51:46 +0000

    all tests OK

--- test/FutballCardsBlindPack.test.js
+++ test/FutballCardsBlindPack.test.js
@@ -5,7 +5,7 @@ const FutballCardsGenerator = artifacts.require('FutballCardsGenerator');
 
 const {BN, constants, expectEvent, shouldFail} = require('openzeppelin-test-helpers');
 
-contract.only('FutballCardsBlindPack', ([_, creator, tokenOwner, anyone, ...accounts]) => {
+contract('FutballCardsBlindPack', ([_, creator, tokenOwner, anyone, ...accounts]) => {
 
     const firstTokenId = new BN(0);
     const secondTokenId = new BN(1);
