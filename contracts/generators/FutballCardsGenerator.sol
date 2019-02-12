f5e2e344df1eed195d2b77d8a90272885aa62f23
Andy Gray <andy@whatsthescore.co.uk>
2019-02-12 08:49:34 +0000

    array adjusting

--- contracts/generators/FutballCardsGenerator.sol
+++ contracts/generators/FutballCardsGenerator.sol
@@ -6,14 +6,14 @@ contract FutballCardsGenerator is Ownable {
 
     uint256 internal randNonce = 0;
 
-    uint256 nameLength = 100;
-    uint256 maxAttributeScore = 100;
+    uint256 public nameLength = 100;
+    uint256 public maxAttributeScore = 100;
 
-    uint256[] nationalities = [0, 1, 2, 3, 4];
-    uint256[] positions = [0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3];
-    uint256[] ethnicity = [0, 0, 0, 0, 1, 1, 1, 2, 2, 3];
-    uint256[] kits = [0, 0, 0, 0, 1, 1, 1, 2, 2, 3];
-    uint256[] colours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    uint256[] internal nationalities = [0, 1, 2, 3, 4];
+    uint256[] internal positions = [0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3];
+    uint256[] internal ethnicity = [0, 0, 0, 0, 1, 1, 1, 2, 2, 3];
+    uint256[] internal kits = [0, 0, 0, 0, 1, 1, 1, 2, 2, 3];
+    uint256[] internal colours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     function generateCard(address _sender)
     external
@@ -61,12 +61,34 @@ contract FutballCardsGenerator is Ownable {
         );
     }
 
+    function allNationalities() public view returns (uint256[] memory) {
+       return nationalities;
+    }
+
     function addNationality(uint256 _new) external onlyOwner returns (bool) {
         nationalities.push(_new);
+        return true;
+    }
+
+    function clearNationalities() external onlyOwner returns (bool) {
+        nationalities.length = 0;
+        return true;
+    }
+
+    function clearNationalityAtIndex(uint256 _index) external onlyOwner returns (bool) {
+        uint lastIndex = nationalities.length - 1;
+        require(_index <= lastIndex);
+
+        nationalities[_index] = nationalities[lastIndex];
+        nationalities.length--;
     }
 
     function addPositions(uint256 _new) external onlyOwner returns (bool) {
         positions.push(_new);
+    }
+
+    function addEthnicity(uint256 _new) external onlyOwner returns (bool) {
+        ethnicity.push(_new);
     }
 
     function addKits(uint256 _new) external onlyOwner returns (bool) {
--- test/FutballCardsGenerator.test.js
+++ test/FutballCardsGenerator.test.js
@@ -4,22 +4,61 @@ const FutballCardsGenerator = artifacts.require('FutballCardsGenerator');
 
 const {BN, constants, expectEvent, shouldFail} = require('openzeppelin-test-helpers');
 
-contract('FutballCardsGenerator tests', (accounts) => {
+contract.only('FutballCardsGenerator tests', (accounts) => {
 
     before(async function () {
-        console.log(accounts);
+        // console.log(accounts);
         this.generator = await FutballCardsGenerator.new({from: accounts[0]});
     });
 
-    it('generate me some randoms', async function () {
-        const results = {};
-        for (let i = 0; i < 100; i++) {
-            const {logs} = await this.generator.generateCard(randomAccount());
+    context('ensure can add to seed arrays', function () {
+        it('adds to nationalities', async function () {
+            let nationalities = await this.generator.allNationalities();
+            nationalities.length.should.be.equal(5);
 
-            const {_nationality, _position, _ethnicity, _kit, _colour} = logs[0].args;
-            console.log(`N ${_nationality} P ${_position} E ${_ethnicity} K ${_kit} C ${_colour}`);
-        }
+            await this.generator.addNationality(new BN('5'), {from: accounts[0]});
+            nationalities = await this.generator.allNationalities();
+            nationalities.length.should.be.equal(6);
+        });
+
+        it('replace nationality at index', async function () {
+            await this.generator.clearNationalityAtIndex(1, {from: accounts[0]});
+            let nationalities = await this.generator.allNationalities();
+            nationalities.length.should.be.equal(5);
+
+            await this.generator.clearNationalityAtIndex(1, {from: accounts[0]});
+            nationalities = await this.generator.allNationalities();
+            nationalities.length.should.be.equal(4);
+        });
+
+        it('replace nationalities', async function () {
+            await this.generator.clearNationalities({from: accounts[0]});
+            let nationalities = await this.generator.allNationalities();
+            nationalities.length.should.be.equal(0);
+        });
+
+        it('only owner can clear nationalities', async function () {
+            await shouldFail.reverting(this.generator.clearNationalities({from: accounts[1]}));
+        });
+
+        it('only owner can clear nationality at index', async function () {
+            await shouldFail.reverting(this.generator.clearNationalityAtIndex(1, {from: accounts[1]}));
+        });
+
+        it('only owner can add to nationalities', async function () {
+            await shouldFail.reverting(this.generator.addNationality(new BN('6'), {from: accounts[1]}));
+        });
     });
+
+    // it('generate me some randoms', async function () {
+    //     const results = {};
+    //     for (let i = 0; i < 100; i++) {
+    //         const {logs} = await this.generator.generateCard(randomAccount());
+    //
+    //         const {_nationality, _position, _ethnicity, _kit, _colour} = logs[0].args;
+    //         console.log(`N ${_nationality} P ${_position} E ${_ethnicity} K ${_kit} C ${_colour}`);
+    //     }
+    // });
 
     function randomAccount () {
         // Random account between 0-9 (10 accounts)
