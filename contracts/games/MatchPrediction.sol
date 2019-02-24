45bec7b8d0bdede19c27f56dc8e3f6c18275ce16
vince0656 <vince0656@googlemail.com>
2019-02-24 22:05:02 +0000

    New functionality added around a match and started laying ground work for an oracle

--- contracts/games/MatchPrediction.sol
+++ contracts/games/MatchPrediction.sol
@@ -4,8 +4,14 @@ import "./abstract/FutballCardGame.sol";
 
 contract MatchPrediction is FutballCardGame {
 
-    enum Outcomes {HOME_WIN, AWAY_WIN, DRAW, ABSTAIN}
+    enum Outcome {UNINITIALISED, HOME_WIN, AWAY_WIN, DRAW}
     enum State {OPEN, PLAYER_1_WIN, PLAYER_2_WIN, DRAW, CLOSED}
+
+    struct Match {
+        uint256 id;
+        uint256 predictFrom;
+        uint256 predictTo;
+    }
 
     struct Game {
         uint256 id;
@@ -13,25 +19,57 @@ contract MatchPrediction is FutballCardGame {
         address p1Address;
         uint256 p2TokenId;
         address p2Address;
-        Outcomes p1Prediction;
-        Outcomes p2Prediction;
+        Outcome p1Prediction;
+        Outcome p2Prediction;
         State state;
+        uint256 matchId;
     }
+
+    address oracle;
 
     Game game;
     uint256 public totalGames = 0;
 
+    mapping(uint256 => uint256) tokenIdToGameIdMapping;
+    mapping(uint256 => Game) gameIdToGameMapping;
+    mapping(uint256 => Outcome) matchIdToResultMapping; // todo: result fn should emit outcome
+    mapping(uint256 => uint256[]) matchIdToOpenGameIdListMapping;
+    mapping(uint256 => Match) matchIdToMatchMapping;
+
     constructor (IFutballCardsAttributes _nft) public {
+        // todo: add validation on all to ensure constructor is not supplied address(0) on any params
+        // todo: should this be done in modifiers?
         nft = _nft;
+    }
+
+    ///////////////
+    // Modifiers //
+    ///////////////
+    modifier onlyWhenOracle() {
+        require(oracle == msg.sender, "matchprediction.validation.error.notoracle");
+        _;
     }
 
     ///////////////
     // Functions //
     ///////////////
 
-    function makeFirstPrediction(uint256 _tokenId, Outcomes _prediction)
+    // todo: add basic validation on the matchId arg using a modifier
+    // todo: add time validation modifiers
+    function addMatch(uint256 _matchId, uint256 _predictFrom, uint256 _predictTo) onlyWhenOracle public {
+        matchIdToMatchMapping[_matchId] = Match({
+            id: _matchId,
+            predictFrom: _predictFrom,
+            predictTo: _predictTo
+        });
+    }
+
+    // todo: add modifier which checks if the Match ID is valid
+    // todo: use inherited token validation modifiers
+    // todo: investigate if prediction needs a validation modifier
+    function makeFirstPrediction(uint256 _matchId, uint256 _tokenId, Outcome _prediction)
     whenNotPaused
-    public {
+    public returns (uint256 _gameId) {
         uint256 newGameId = totalGames.add(1);
 
         game = Game({
@@ -41,17 +79,20 @@ contract MatchPrediction is FutballCardGame {
             p2TokenId: 0,
             p2Address: address(0),
             p1Prediction: _prediction,
-            p2Prediction: Outcomes.ABSTAIN,
-            state: State.OPEN
+            p2Prediction: Outcome.UNINITIALISED,
+            state: State.OPEN,
+            matchId: _matchId
         });
 
         totalGames = totalGames.add(1);
 
-        // todo: Emit a game created event and return a Game ID
+        // todo: Emit a game created event
+
+        return newGameId;
     }
 
-    function wasPredictionTrue()  public view returns (bool) {
-        Outcomes fixedResult = Outcomes.HOME_WIN;
+    function wasPredictionTrue() public view returns (bool) {
+        Outcome fixedResult = Outcome.HOME_WIN;
         return fixedResult == game.p1Prediction;
     }
 
@@ -79,4 +120,6 @@ contract MatchPrediction is FutballCardGame {
     function _isTokenNotAlreadyPlaying(uint256 _tokenId) internal returns (bool) {
         return false;
     }
+
+    // todo: add update oracle address function
 }
--- test/games/MatchPrediction.test.js
+++ test/games/MatchPrediction.test.js
@@ -3,16 +3,19 @@ const MatchPrediction = artifacts.require('MatchPrediction');
 
 const {BN, constants, expectEvent, shouldFail} = require('openzeppelin-test-helpers');
 
-contract.only('Match Prediction Contract Tests', ([_, creator, tokenOwner1, tokenOwner2, anyone, ...accounts]) => {
+contract.only('Match Prediction Contract Tests', ([_, creator, tokenOwner1, tokenOwner2, oracle, ...accounts]) => {
     const baseURI = 'http://futball-cards';
     const Outcomes = {
-        HOME_WIN: new BN(0),
-        AWAY_WIN: new BN(1),
-        DRAW: new BN(2)
+        UNINITIALISED: new BN(0),
+        HOME_WIN: new BN(1),
+        AWAY_WIN: new BN(2),
+        DRAW: new BN(3)
     };
 
     const _tokenId1 = new BN(0);
     const _tokenId2 = new BN(1);
+
+    const _matchId = new BN(34564543);
 
     before(async () => {
         this.futballCards = await FutballCards.new(baseURI, {from: creator});
@@ -25,7 +28,7 @@ contract.only('Match Prediction Contract Tests', ([_, creator, tokenOwner1, toke
 
         it('should handle a basic prediction', async () => {
             // todo: Extend this by minting a card and checking the onlyWhenTokenOwner guard and others work
-            await this.matchPrediction.makeFirstPrediction(_tokenId1, Outcomes.HOME_WIN);
+            await this.matchPrediction.makeFirstPrediction(_matchId, _tokenId1, Outcomes.HOME_WIN);
 
             (await this.matchPrediction.wasPredictionTrue()).should.be.true;
         });
