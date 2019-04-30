763a2686c0f9bc37f96b2e8f66dfc8b90a256db3
vince0656 <vince0656@googlemail.com>
2019-04-30 19:56:06 +0100

    Added Match contracts to flattener

--- truffle_flatten.sh
+++ truffle_flatten.sh
@@ -9,3 +9,7 @@ node ./node_modules/.bin/truffle-flattener ./contracts/FutballCardsBlindPack.sol
 node ./node_modules/.bin/truffle-flattener ./contracts/BuyNowMarketplace.sol > ./contracts-flat/BuyNowMarketplace.sol;
 
 node ./node_modules/.bin/truffle-flattener ./contracts/games/HeadToHead.sol > ./contracts-flat/HeadToHead.sol;
+
+node ./node_modules/.bin/truffle-flattener ./contracts/service/MatchService.sol > ./contracts-flat/MatchService.sol;
+
+node ./node_modules/.bin/truffle-flattener ./contracts/games/MatchPrediction.sol > ./contracts-flat/MatchPrediction.sol;
