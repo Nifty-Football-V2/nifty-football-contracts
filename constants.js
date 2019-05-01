73330c5601eae21439474b3ed20142003b1451e0
vince0656 <vince0656@googlemail.com>
2019-05-01 21:11:38 +0100

    Using env variables directly

--- constants.js
+++ constants.js
@@ -1,13 +1,10 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
-const INFURA_KEY = process.env.NIFTY_FOOTBALL_INFURA_KEY;
-const mnemonic = process.env.NIFTY_FOOTBALL_MNEMONIC;
-
-
 function getAccountAddress(accounts, index, network) {
     let addr = accounts[index];
     if (network === 'ropsten' || network === 'rinkeby') {
-        addr = new HDWalletProvider(mnemonic, `https://${network}.infura.io/v3/${INFURA_KEY}`, index).getAddress();
+        addr = new HDWalletProvider(process.env.NIFTY_FOOTBALL_MNEMONIC,
+            `https://${network}.infura.io/v3/${process.env.NIFTY_FOOTBALL_INFURA_KEY}`, index).getAddress();
     }
     console.log(`Using account [${addr}] for network [${network}]`);
     return addr;
